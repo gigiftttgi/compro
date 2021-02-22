e7ac95bda7788f5b8ca2d1a53197372f70fd2f1e
Jongkonrat <jongkonrat_pisit@cmu.ac.th>
2021-02-22 15:07:24 +0700

    เขียนcomment ไว้แล้ว

--- showbill.cpp
+++ showbill.cpp
@@ -9,119 +9,24 @@ using namespace std;
 int num, booklist, mt;
 string name;
 
-void bill()
-{
-	string name;
-	cout << "Enter name : ";
-	getline(cin,name);
-	cout << "Seat : ";
-	for(int i=0;i<num;i++)
-	cout << booklist[i] << " ";
-	fstream source;
-	switch(mt)
-	{
-		case 2 : 
-				 source.open("Bill_m1t1.txt", ios::app); 
-				 source << "Name : " << name << "\n";
-				 for(int j=0;j<num;j++)
-				 {
-					source << booklist[j] << " ";
-				 } 
-				 source.close();
-				 //home();
-				 break;
-		case 3 : 
-				 source.open("Bill_m1t2.txt", ios::app); 
-				 source << "Name : " << name << "\n";
-				 for(int j=0;j<num;j++)
-				 {
-					source << booklist[j] << " ";
-				 } 
-				 source.close();
-				 //home();
-				 break;
-		case 4 : 
-				 source.open("Bill_m1t3.txt", ios::app); 
-				 source << "Name : " << name << "\n";
-				 for(int j=0;j<num;j++)
-				 {
-					source << booklist[j] << " ";
-				 } 
-				 source.close();
-				 //home();
-				 break;
-		case 5 : 
-				 source.open("Bill_m2t1.txt", ios::app); 
-				 source << "Name : " << name << "\n";
-				 for(int j=0;j<num;j++)
-				 {
-					source << booklist[j] << " ";
-				 } 
-				 source.close();
-				 //home();
-				 break;
-		case 6 : 
-				 source.open("bill_m2t2.txt", ios::app); 
-				 source << "Name : " << name << "\n";
-				 for(int j=0;j<num;j++)
-				 {
-					source << booklist[j] << " ";
-				 } 
-				 source.close();
-				 //home();
-				 break;
-		case 7 : 
-				 source.open("Bill_m2t3.txt", ios::app); 
-				 source << "Name : " << name << "\n";
-				 for(int j=0;j<num;j++)
-				 {
-					source << booklist[j] << " ";
-				 } 
-				 source.close();
-				 //home();
-				 break;
-		case 10 : 
-				 source.open("Bill_m3t1.txt", ios::app); 
-				 source << "Name : " << name << "\n";
-				 for(int j=0;j<num;j++)
-				 {
-					source << booklist[j] << " ";
-				 } 
-				 source.close();
-				 //home();
-				 break;
-		case 11 : 
-				 source.open("Bill_m3t2.txt", ios::app); 
-				 source << "Name : " << name << "\n";
-				 for(int j=0;j<num;j++)
-				 {
-					source << booklist[j] << " ";
-				 } 
-				 source.close();
-				 //home();
-				 break;
-		case 12 : 
-				 source.open("Bill_m3t3.txt", ios::app); 
-				 source << "Name : " << name << "\n";
-				 for(int j=0;j<num;j++)
-				 {
-					source << booklist[j] << " ";
-				 } 
-				 source.close();
-				 //home();
-				 break;
-	}	
-	//home();
+
+//ไม่มั่นใจว่าต้องเอาฟังก์ชั่นนี้ไปใส่ตรงไหน เลยเอาแยกไว้ตรงนี้ก่อน 
+void showbill(){
+	cout << setw(30) << "CPE THEATER\n";
+    cout << bill(); //เห็นว่าในฟังก์ชั่นนี้มีให้ใส่ชื่อกับที่นั่ง เลยดึงมาใช้เลย
+    cout << "Movie : " << ch_movie;
+    cout << "Show time : " << ch_time;
+	cout << setw(30) << "Thank you";
 }
 
+//กรณีที่สองถ้าหากว่าอยากอ่านข้อมูลใน.txt แทนก็ได้ แต่เราไม่รู้ว่าต้องไฟล์.txt ไหน เครื่องเราไม่มีงับ เลยทำเป็นแบบฟอร์มให้เห็นเป็นแบบนี้ก่อน
 void showbill(){
     fstream source;
-    source.open("seat.txt"); //ไม่มั่นใจว่าเพื่อนใช้ชื่ออะไร เพราะไม่ได้push ลงมางับ ตรงนี้ไว้ค่อยเปลี่ยน
+    source.open(""); //ไม่มั่นใจว่าเพื่อนใช้ชื่ออะไร เพราะไม่ได้push ลงมางับ ตรงนี้ไว้ค่อยเปลี่ยน
     string textline;
-    //หลังจากตรงนี้สร้างอ่านจากไฟล์.txt ที่ไปเก็บไว้ได้ แล้วค่อยcout ออกมา
+    //หลังจากตรงนี้สร้างอ่านจากไฟล์.txt ที่ไปเก็บไว้ได้
 
-    cout << setw(40) << "CPE THEATER\n";
-    cout << bill(); //เพราะมีการให้user enter name
+	//คิดว่าแบบที่สองน่าจะสะดวกกว่าไหม เพราะยังไง ก็มีการเก็บข้อมูลไว้อยู่แล้ว
 }
 
 int main(){
