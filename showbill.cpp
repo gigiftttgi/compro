ba96dc7744b9986f9fc0d1b0548823341ab484e6
Jongkonrat <jongkonrat_pisit@cmu.ac.th>
2021-02-28 09:00:08 +0700

    เหลือจำนวนเงิน

--- showbill.cpp
+++ showbill.cpp
@@ -3,16 +3,15 @@
 #include<iomanip>
 using namespace std;
 
-//ไม่มั่นใจว่าต้องเอาฟังก์ชั่นนี้ไปใส่ตรงไหน เลยเอาแยกไว้ตรงนี้ก่อน 
+
 void showbill(){
 	cout << setw(30) << "CPE THEATER\n";
-    cout << bill(); //เห็นว่าในฟังก์ชั่นนี้มีให้ใส่ชื่อกับที่นั่ง เลยดึงมาใช้เลย ไม่รู้ว่าอันไหนคือตัวแปรของชื่อผู้ใช้และที่นั่งที่เลือกไว้
+    cout << "Name : " << name;
     cout << "Movie : " << ch_movie;
     cout << "Show time : " << ch_time;
+    cout << "Seat : " << row << seatnum;
 	cout << setw(30) << "Thank you";
 }
-
-//กรณีที่สองถ้าหากว่าอยากอ่านข้อมูลใน.txt แทนก็ได้ แต่เราไม่รู้ว่าต้องไฟล์.txt ไหนงับเพราะเหมือนจะไม่ได้pushลงมารึเปล่า
 
 int main(){
     showbill();
