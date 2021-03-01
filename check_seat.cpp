b2d742fbb59a8422b9a59132a7124d67ff1f92ca
minmeeen <76035644+minmeeen@users.noreply.github.com>
2021-03-01 13:48:19 +0700

    update seats movie time

--- check_seat.cpp
+++ check_seat.cpp
@@ -1,19 +1,37 @@
 void valiable_seat(int mt){
-	string s[30];
+	string s[60];
 	string textline;
     string seat[30] = {"A1", "A2", "A3", "A4"," "," ","B1", "B2", "B3", "B4", "B5", "B6", "C1", "C2", "C3", "C4", "C5", "C6", "D1", "D2", "D3", "D4", "D5", "D6", "E1", "E2", "E3", "E4", "E5", "E6"};
 	int k = 0;
 	ifstream source;
 	switch(mt){
-		case 2 : source.open("Booklist_m1t1.txt"); break;
-		case 3 : source.open("Booklist_m1t2.txt"); break;
-		case 4 : source.open("Booklist_m1t3.txt"); break;
-		case 5 : source.open("Booklist_m2t1.txt"); break;
-		case 6 : source.open("Booklist_m2t2.txt"); break;
-		case 7 : source.open("Booklist_m2t3.txt"); break;
-		case 10 : source.open("Booklist_m3t1.txt"); break;
-		case 11 : source.open("Booklist_m3t2.txt"); break;
-		case 12 : source.open("Booklist_m3t3.txt"); break;
+        case 2 : cout << "Movie : 1\n";
+				cout << "Time : 1 | 11:00 - 13:00 |\n"; 
+				source.open("Booklist_m1t1.txt"); break;
+		case 3 : cout << "Movie : 1\n";
+				cout << "Time : 2 | 14:30 - 16:30 |\n"; 
+				source.open("Booklist_m1t2.txt"); break;
+		case 4 : cout << "Movie : 1\n";
+				cout << "Time : 3 | 18:30 - 20:30 |\n"; 
+				source.open("Booklist_m1t3.txt"); break;
+		case 5 : cout << "Movie : 2\n";
+				cout << "Time : 1 | 11:30 - 13:30 |\n"; 
+				source.open("Booklist_m2t1.txt"); break;
+		case 6 : cout << "Movie : 2\n";
+				cout << "Time : 2 | 15:00 - 17:00 |\n"; 
+				source.open("Booklist_m2t2.txt"); break;
+		case 7 : cout << "Movie : 2\n";
+				cout << "Time : 3 | 19:00 - 21:00 |\n"; 
+				source.open("Booklist_m2t3.txt"); break;
+		case 10 : cout << "Movie : 3\n";
+				cout << "Time : 1 | 12:00 - 14:00 |\n"; 
+				source.open("Booklist_m3t1.txt"); break;
+		case 11 : cout << "Movie : 3\n";
+				cout << "Time : 2 | 15:30 - 17:30 |\n"; 
+				source.open("Booklist_m3t2.txt"); break;
+		case 12 : cout << "Movie : 3\n";
+				cout << "Time : 3 | 19:30 - 21:30 |\n"; 
+				source.open("Booklist_m3t3.txt"); break;
 	}
 
 	while(getline(source,textline)){
@@ -21,6 +39,7 @@ void valiable_seat(int mt){
 		k++;  
     }
 	source.close();
+	
 	int N= k/2;
     string out[N];
     int x=0;
@@ -32,6 +51,10 @@ void valiable_seat(int mt){
        	x++;    	
 	}
 	
+	
+	
+	
+
     for(int i =0 ; i<30 ; i++){
         for(int j=0 ; j<x ; j++){
             if(out[j] == seat[i]){
@@ -40,8 +63,7 @@ void valiable_seat(int mt){
         }
     }
     
- 	cout << "Valiable Seats";
-
+ 	cout << "<Valiable Seats>";
     for(int i=0; i<30 ; i++){
 		if(i%6 == 0){
 			cout << endl ;
@@ -51,6 +73,12 @@ void valiable_seat(int mt){
             cout << seat[i] << "  ";
 
 		}
+	}
+	cout << "\nTotal : " << 28-x ;
+	if(28-x >1){
+		cout << " seats";
+	}else{
+		cout << "seat";
 	}
     
     
