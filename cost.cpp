b914a09e056b1d7231c90327ba9df58471af6eef
Jongkonrat <jongkonrat_pisit@cmu.ac.th>
2021-03-04 21:17:03 +0700

    demo

---
+++ cost.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+using namespace std;
+
+int numofseat = 2;
+float cost = 0;
+char r = 'a';
+int sn = 2;
+string discount = "Hello";
+
+void costs(){
+    for(int i=0;i<numofseat;i++)
+	{
+		cout << (r+i) << (sn+i) << " ";
+		if((r+i) == 'A' or (r+i) == 'a')
+		{
+			cost+=180;
+		}
+		else cost+=120;
+	}
+	//cout << "\nTotal ticket cost : " << cost;
+}
+
+void code(){
+    bool user;
+    string user_code;
+    char key;
+    do{
+        cout << "Do you have discount (Y or N) : ";
+        cin >> key;
+        if(key == 'y' || key == 'Y'){
+            cout << "Enter code : ";
+            cin >> user_code;
+            if(discount == user_code){
+                cout << "Total ticket cost : " << cost - (cost*10)/100 << "Bath.";
+            }
+            user = true;
+        }
+        else if(key =='n' || key == 'N'){
+            cout << "Total ticket cost : " << cost << "Bath.";
+            user = true;
+        }
+        else{
+            cout << "Please try again";
+            user = false;
+        }
+    }while(user == false);
+    //costs();
+}
+
+int main(){
+    code();
+    return 0;
+}
